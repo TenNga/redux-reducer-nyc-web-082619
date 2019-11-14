8ffbc94c9c07b62f56f2b601925b92ca4a554761
Kevin Karma <kevinkarma55@gmail.com>
2019-11-14 18:16:09 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,4 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends = []}, action){
+  if(action.type === "ADD_FRIEND")
+    return {friends: [...state.friends, action.friends]}
 }
