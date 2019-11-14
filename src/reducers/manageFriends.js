ffd350b742a239bff992b6756a47ae1c3a1e5a6e
Kevin Karma <kevinkarma55@gmail.com>
2019-11-14 18:26:31 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,7 +2,7 @@ export function manageFriends(state={friends: []}, action){
   if(action.type === "ADD_FRIEND")
     return {friends: [...state.friends, action.friend]}
   if(action.type === "REMOVE_FRIEND")
-    return {friends: state.filter(friend => friend.id !== action.id)}
+    return {friends: state.friends.filter(friend => friend.id !== action.id)}
   else 
     return state;
 }
