4d156e7ba1c1db79cfd5c50b573e86ae016a5fcf
Kevin Karma <kevinkarma55@gmail.com>
2019-11-14 18:17:57 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,4 +1,6 @@
 export function managePresents(state={numberOfPresents: 0}, action){
   if(action.type === "INCREASE")
     return {numberOfPresents: state.numberOfPresents + 1}
+  else 
+    return state;
 }
